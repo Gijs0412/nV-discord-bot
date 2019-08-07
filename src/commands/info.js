97a4ac2eb436e749d9d22ce07e06114e900ff735
Byto <42752426+Bytooo@users.noreply.github.com>
2019-08-06 22:15:06 -0500

    Change: infoEmbed color

--- src/commands/info.js
+++ src/commands/info.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
     let infoEmbed = new Discord.RichEmbed()
         .setTitle('Bot Information')
-        .setColor('#ffffff')
+        .setColor('#7a0ce8')
         .addField('Language', 'JavaScript')
         .addField('Library', 'discord.js')
         .addField('About nVBot', 'nVbot is the official bot for the nV krunker.io clan.')
