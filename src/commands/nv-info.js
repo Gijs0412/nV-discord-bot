273b637cd65b1e2d7e5eb60c8905c906eaa372c5
Byto <42752426+Bytooo@users.noreply.github.com>
2019-08-06 22:49:58 -0500

    Add: nV clan info

---
+++ src/commands/nv-info.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (client, message, args) => {
+    let nvInfoEmbed = new Discord.RichEmbed()
+        .setTitle('Clan Information')
+        .setColor('#7a0ce8')
+        .addField('Server Owner', `${message.guild.owner}`)
+        .addField('Clan Owner(s)', 'Zahseh, Gaius, Tahha, Zimcro')
+        .addField('Discord Server Creation Date' `${message.guild.createdAt}`)
+        .addField('In-game Clan Creation Date', 'nV was created at 3/03/2019')
+        .addField('Clan Description', 'nV is the top competitive clan in krunker.io, we got 1st place in Krunker Global winning a total of 1.4k dollars and won an overall amount of 1,587 dollars. nV #1')
+    return message.channel.send(nvInfoEmbed);
+};
+
+module.exports.help = {
+    name: 'nv-info'
+};
