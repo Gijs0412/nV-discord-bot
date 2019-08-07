6687e213fa961aafa69a83ae7521d3e7392447c4
Byto <42752426+Bytooo@users.noreply.github.com>
2019-08-06 22:56:22 -0500

    Add: line jumps for nvInfoEmbed

--- src/commands/nv-info.js
+++ src/commands/nv-info.js
@@ -4,10 +4,10 @@ module.exports.run = async (client, message, args) => {
     let nvInfoEmbed = new Discord.RichEmbed()
         .setTitle('Clan Information')
         .setColor('#7a0ce8')
-        .addField('Server Owner', `${message.guild.owner}`)
-        .addField('Clan Owner(s)', 'Zahseh, Gaius, Tahha, Zimcro')
-        .addField('Discord Server Creation Date', `${message.guild.createdAt}`)
-        .addField('In-game Clan Creation Date', 'nV was created at 3/03/2019')
+        .addField('Server Owner', `${message.guild.owner}\n`)
+        .addField('Clan Owner(s)', 'Zahseh, Gaius, Tahha, Zimcro\n')
+        .addField('Discord Server Creation Date', `${message.guild.createdAt}\n`)
+        .addField('In-game Clan Creation Date', 'nV was created at 3/03/2019\n')
         .addField('Clan Description', 'nV is the top competitive clan in krunker.io, we got 1st place in Krunker Global winning a total of 1.4k dollars and won an overall amount of 1,587 dollars. nV #1')
     return message.channel.send(nvInfoEmbed);
 };
