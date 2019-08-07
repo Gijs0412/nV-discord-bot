f61720f78712b2aebef3f984781cbec3447b021b
Byto <42752426+Bytooo@users.noreply.github.com>
2019-08-07 00:01:51 -0500

    Fix: prefix

--- index.js
+++ index.js
@@ -29,13 +29,11 @@ client.on('message', async message => {
     if(message.author.bot) return;
     if(message.channel.type === 'dm') return;
 
-    let prefix = 'nv '
+    let prefix = '$'
     let messageArray = message.content.split(' ');
     let command = messageArray[0];
     let args = command.slice(1);
-    console.log(command);
-    console.log(command.slice(prefix.length));
-    console.log(args);
+    
     let commandFile = client.commands.get(command.slice(prefix.length));
     if(commandFile) commandFile.run(client, message, args);
 });
